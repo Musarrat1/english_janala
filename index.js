569e3441d3932877dc26acbe596db3c699c7ce96
Musarrat1 <junuarabegum8@gmail.com>
2025-06-15 21:53:55 +0600

    added js of lessons

---
+++ index.js
@@ -0,0 +1,20 @@
+function loadLevel() {
+    fetch('https://openapi.programming-hero.com/api/levels/all')
+        .then(res => res.json())
+        .then((data) => displayLevel(data.data));
+}
+
+function displayLevel(level) {
+    const levelContainer = document.getElementById("level-container");
+    levelContainer.innerHTML='';
+    for (let i of level) {
+        const levelDiv = document.createElement("div");
+        levelDiv.innerHTML = `
+       
+         <button class="btn btn-outline btn-primary">
+                   <img src="assets/fa-book-open.png" alt="" srcset="" class="hover:text-white ">Lesson-${i.level_no}</button>
+                  `;
+        levelContainer.append(levelDiv);
+    }
+}
+loadLevel();
